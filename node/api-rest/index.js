306839ba4e11ca0b4010c4b1f3c77132a48e84a7
jmf62 <jmf62@alu.ua.es>
2023-02-06 19:56:23 +0100

    practica1

--- node/api-rest/index.js
+++ node/api-rest/index.js
@@ -1,11 +1,68 @@
 'use strict'
-const port = process.env.PORT || 8080; 
-const express = require('express'); 
-const app = express(); 
+const port = process.env.PORT || 3000;
 
-app.get('/hola/:unNombre', (req, res) => { 
-    res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` }); 
-}); 
-app.listen(port, () => { 
+const { application } = require('express');
+const express = require('express');
+
+const logger = require('morgan');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended:false}));
+app.use(express.json());
+
+app.get('/api/product', (req, res) => {
+
+    res.status(200);
+    res.send({ productos: [] });
+
+});
+
+app.get('/api/product/:productID', (req, res) =>{
+
+    const productID = req.params.productID; 
+    res.status(200); 
+    res.send({product: productID});
+
+});
+
+app.post('/api/product', (req, res) => {
+
+    const queProducto = req.body; 
+    console.log(queProducto); 
+    
+    res.status(200); 
+    res.send({
+        mensaje: 'Producto Creado', 
+        producto: queProducto
+    });
+
+});
+
+app.put('/api/product/:productID', (req, res) => {
+    const queProducto = req.body; 
+    const productID = req.params.productID;
+
+    res.status(200);
+    res.send({
+        mensaje: `Producto Modificado  ${productID}`, 
+        producto: queProducto
+    });
+
+});
+
+app.delete('/api/product/:productID', (req, res) => {
+
+    const productID = req.params.productID;
+
+    res.status(200); 
+    res.send({ mensaje: `Producto eliminado: ${productID}`});
+
+});
+
+
+app.listen(port, () => {
+
     console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
-}); 
+
+});
